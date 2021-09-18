5cd6db69b53ac40b493915a2f33a15de6c455f51
great <great@mxsoft.com>
2021-09-18 17:46:22 +0800

    db层新增mq支持

--- DB.py
+++ DB.py
@@ -2,6 +2,7 @@
 # @Create   : 2021/8/19 14:56
 # @Author   : yh
 # @Remark   : mx框架db层
+from .mqDB import MQ
 from .TableDB import TableDB
 from .TreeDB import TreeDB
 
@@ -11,3 +12,4 @@ class DB:
     def __init__(self, *args, **kwargs):
         self.tree = TreeDB()
         self.table = TableDB()
+        self.mq = MQ()
