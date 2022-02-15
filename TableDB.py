1ea9a9dfd4ab8d488ee56d28d85bf95295ea5770
Neymar <552513069@qq.com>
2022-02-15 15:59:12 +0800

    fix(TableDB.py) 增加table表sql查询

--- TableDB.py
+++ TableDB.py
@@ -8,11 +8,11 @@ from typing import Type, List, Any
 
 from superbsapi import CBSHandleLoc
 
-from . import Model
 from .BaseDB import BaseDB
 from .db_def.def_table import *
 from .db_def.def_type import type_map, type_c_python, type_c_str
 from .exception import DBError, DataError
+from . import Model
 
 symbol_map = {
     'e': TBDB_FMC_EQUAL,  # 等于
@@ -25,6 +25,18 @@ symbol_map = {
     # 其他的类型，以后用到了再添加
 }
 
+sql_symbol_map = {
+    'ne': '!=',  # 不等于
+    'gt': '>',  # 大于
+    'lt': '<',  # 小于
+    'gte': '>=',  # 大于等于
+    'lte': '<=',  # 小于等于
+    'like': 'like',  # 模糊匹配，使用 * 做为通配符
+    'in': 'in',  # 范围查找（枚举）
+    'between': 'between',  # 范围查找（范围）
+    # sql中支持limit,   # eq:limit a,b 从第a个开始，查询b条数据
+    # 其他的类型，数据库支持了再添加
+}
 
 class TableDB(BaseDB):
 
@@ -32,7 +44,7 @@ class TableDB(BaseDB):
         super().__init__(host, port)
         self.__table = None
 
-    def open(self, file: str, table: str = None, host: str = None, port: int = None):
+    def open(self, file: str, table: str = None, host: str = None, port: str = None):
         """
         打开table数据库
         :param file: 数据库名称
@@ -90,19 +102,19 @@ class TableDB(BaseDB):
         :param flag: 创建风格，不传会自动判断
         :param kwargs: 附加参数
         """
-        flag = 0x00000000
-        if kwargs.get('auto_id'):
-            flag += BS_TDTF_FLAG_AUTOID
-        if kwargs.get('auto_id_index'):
-            flag += BS_TDTF_FLAG_INDEXID
-        if kwargs.get('primary_key'):
-            flag += BS_TDTF_FLAG_PRIMARYTABLE
-        if kwargs.get('desc'):
-            flag += BS_TDTF_FLAG_PMKI_DESCORDER
-        if kwargs.get('dyn'):
-            flag += BS_TDTF_FLAG_DYNTABLE
-        if not flag:
-            flag = BS_TDTF_FLAG_AUTOID
+        # flag = 0x00000000
+        # if kwargs.get('auto_id'):
+        #     flag += BS_TDTF_FLAG_AUTOID
+        # if kwargs.get('auto_id_index'):
+        #     flag += BS_TDTF_FLAG_INDEXID
+        # if kwargs.get('primary_key'):
+        #     flag += BS_TDTF_FLAG_PRIMARYTABLE
+        # if kwargs.get('desc'):
+        #     flag += BS_TDTF_FLAG_PMKI_DESCORDER
+        # if kwargs.get('dyn'):
+        #     flag += BS_TDTF_FLAG_DYNTABLE
+        # if not flag:
+        #     flag = BS_TDTF_FLAG_AUTOID
 
         field_list = list()
         for field in model.__fields__.values():
@@ -187,15 +199,6 @@ class TableDB(BaseDB):
 
         self.exec_tree('Tabledb_SelectRecordsByCTime', self.__table, count, start_time, end_time, res)
         return res
-    
-    def exec_for_sql(self, sql: str) -> List[dict]:
-        """ 
-        根据sql查询数据
-        :param sql: 查询sql
-        """
-        res_list = []
-        self.exec_tree('Tabledb_Query', sql, res_list)
-        return res_list
 
     def filter(self, table_name: str = None, count: int = 0, **kwargs) -> List[dict]:
         """
@@ -239,3 +242,123 @@ class TableDB(BaseDB):
                        res_list)
 
         return res_list
+
+    def exec_for_sql(self, sql: str) -> List[dict]:
+        """
+        根据sql查询数据
+        :param sql: 查询sql
+        """
+        res_list = []
+        self.exec_tree('Tabledb_Query', sql, res_list)
+        return res_list
+
+    def sql_insert(self, table: str, keys: tuple, values: List[tuple]):
+        """
+        插入数据
+        :param table:数据库表，必传
+        :param keys:插入的字段名，需传表中所有字段，必传
+        :param values: 插入的字段值，必传
+        eq:  db.insert(db,(a,b,c),[(1,2,3),(1,2,3)]
+        """
+        if not isinstance(keys, tuple):
+            raise DataError('错误的数据类型，keys应为元组')
+
+        if not isinstance(values, list):
+            raise DataError('错误的数据类型，values应为列表，其中的元素多个字段应为元组，单个字段应为字符串')
+
+        if len(keys) != len(values[0]):
+            if not isinstance(values[0], tuple):
+                raise DataError('错误的数据类型，values应为列表，其中的元素多个字段应为元组，单个字段应为字符串或数字')
+            raise DataError('keys数量与value数量不符，eq: db.insert(db,(a,b,c),[(1,2,3),(1,2,3)]')
+
+        keys = ','.join(keys)
+        sql = f"insert into {table} ({keys}) values {','.join([str(i) for i in values])}"
+
+        return self.exec_for_sql(sql)
+
+    def sql_filter(self, table: str,
+               prop_list=None,
+               page_size: int = None,
+               page_index: int = None,
+               count: int = None,
+               default_expression: str = None,
+               # is_desc: bool = True,
+               filter_str: str = None, **kwargs):
+        """
+        根据属性筛选符合条件的子键（可分页显示）
+        :param prop_list:筛选需查询的字段，不传查所有
+        :param table:数据库表，必传
+        :param filter_str: sql中where后的语句，输入直接按照sql语句查询
+        :param kwargs: 传入的查询条件
+        :param page_size：每页条数
+        :param page_index：第几页
+        :param count: 查询条数
+        :param default_expression：手动传入条件关系，默认关系为 and
+        :return: 总条数，查询结果
+        """
+        if prop_list is None:
+            prop_list = []
+        if isinstance(prop_list, str):
+            prop_list = [prop_list]
+
+        prop = ','.join([str(i) for i in prop_list]) or '*'
+
+        sql = f"select {prop} from {table}"
+        count_sql = f"select count(*) from {table}"
+
+        if filter_str:
+            sql += ' where ' + filter_str
+        else:
+            query_list = list()
+
+            for key, value in kwargs.items():
+                try:
+                    key, symbol = key.rsplit('__', 1)
+                    if symbol in sql_symbol_map:
+                        symbol = sql_symbol_map.get(symbol)
+                    else:
+                        raise DataError('查询操作错误！正确操作包含：gt、lt等')
+                except ValueError:
+                    symbol = '='
+
+                if symbol == 'in':
+                    if not isinstance(value, tuple):
+                        raise DataError('查询格式错误！正确示例：a__in=(1, 3, 4, 5)')
+                    if len(value) == 0:
+                        raise DataError('使用in时元组不能为空')
+
+                elif symbol == 'between':
+                    if not isinstance(value, list) or len(value) != 2:
+                        raise DataError('查询格式错误！正确示例：a__between=[1, 3]，详情见TreeModel使用手册')
+                    value = ' and '.join([str(i) for i in value])
+
+                if value:
+                    query_list.append(f"{key} {symbol} {value}")
+
+            if default_expression:
+                expression_list = re.split('\d', default_expression)[1:-1]
+                if expression_list:
+                    for index, value in enumerate(query_list):
+                        expression_list.insert(0 + index * 2, value)
+
+                    expression = ' '.join(expression_list)
+                else:
+                    raise DataError("查询格式错误！正确示例：a__default_expression='0 and 1 or 2'，详情见TreeModel使用手册")
+            else:
+                expression = ' and '.join(query_list)
+
+            sql += ' where ' + expression
+            count_sql += ' where ' + expression
+
+            if count:
+                limit_list = [str(0), str(count)]
+                limit_sql = ' limit ' + ','.join(limit_list)
+                sql += limit_sql
+            elif page_size:
+                limit_list = [str((page_index-1)*page_size),str(page_size)]
+                limit_sql = ' limit ' + ','.join(limit_list)
+                sql += limit_sql
+
+        total = self.exec_for_sql(count_sql)[0]['count(*)']
+
+        return total, self.exec_for_sql(sql)
