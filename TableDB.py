45407e1795a13608f2b5c3fae3df40022f3318d0
great <great.yuan@mxsoft.com>
2022-03-08 11:35:30 +0800

    修改table查询方法

--- TableDB.py
+++ TableDB.py
@@ -261,4 +261,4 @@ class TableDB(BaseDB):
         print(sql)
         total = self.exec_for_sql(count_sql)[0]['count(*)']
 
-        return count or total, self.exec_for_sql(sql)
+        return min(count, total), self.exec_for_sql(sql)
